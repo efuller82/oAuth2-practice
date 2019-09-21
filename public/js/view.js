889c3bcabbf8a3571e760baeda796eed9889ad8f
JoJo <jae.kae.tea@gmail.com>
2019-09-20 20:56:53 -0500

    testing

--- public/js/view.js
+++ public/js/view.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+///
+//begin script for oauth
+
+$('#modal1').modal();
+
+function onSignIn(googleUser) {
+    // hides modal if user is signed in
+    $('#modal1').modal('hide');
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    //console.log('Given Name: ' + profile.getGivenName());
+    //console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    // The ID token you need to pass to your backend:
+    // var id_token = googleUser.getAuthResponse().id_token;
+    //console.log("ID Token: " + id_token);
+};
+
+
+
+
+
+   
+///
 
 var $newArtist;
 var $newSong;
@@ -110,26 +137,6 @@ function showSongs(data) {
     $("#makeReview").css("visibility", "visible");
     getReviews();
 };
-
-//begin script for oauth
-
-$('#modal1').modal();
-
-function onSignIn(googleUser) {
-    // hides modal if user is signed in
-    $('#modal1').modal('hide');
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    //console.log('Given Name: ' + profile.getGivenName());
-    //console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    // The ID token you need to pass to your backend:
-    // var id_token = googleUser.getAuthResponse().id_token;
-    //console.log("ID Token: " + id_token);
-}
 
 var $reviewContainer = $(".review-container");
 
